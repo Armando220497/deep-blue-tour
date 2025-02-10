2b690ce632cb6429ff1a2c6ed3c7fc00790f23f5
Armando220497 <armando.di.santo@hotmail.com>
2025-02-10 16:56:19 +0100

    Modifica guest.guard.ts: aggiornamenti alla logica di protezione delle pagine

--- src/app/auth/guest.guard.ts
+++ src/app/auth/guest.guard.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
-// GuestGuard is used to prevent access to the login page if the user is already logged in.
-// If the user has a userToken in localStorage, they are redirected to the home page.
-
 @Injectable({
   providedIn: 'root',
 })
@@ -11,12 +8,10 @@ export class GuestGuard implements CanActivate {
   constructor(private router: Router) {}
 
   canActivate(): boolean {
-    const isLoggedIn = !!localStorage.getItem('userToken'); // Checks if the user is logged in by looking for a token in localStorage
-
-    if (isLoggedIn) {
-      this.router.navigate(['/home']); // If the user is logged in, redirects to the home page
-      return false; // Prevents access to the login page
+    if (typeof window !== 'undefined' && localStorage.getItem('userToken')) {
+      this.router.navigate(['/home']);
+      return false;
     }
-    return true; // Allows access to the login page if the user is not logged in
+    return true;
   }
 }

cf580163c05c2c49daf57c72ae8c4bd6b1996841
Armando220497 <armando.di.santo@hotmail.com>
2025-02-10 16:57:04 +0100

    Modifica about.component.ts: aggiornamenti alla logica del componente

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  AfterViewInit,
+  ViewChild,
+  Inject,
+  PLATFORM_ID,
+} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -6,4 +14,31 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {}
+export class AboutComponent implements AfterViewInit {
+  @ViewChild('aboutContainer') aboutContainer!: ElementRef;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      if ('IntersectionObserver' in window) {
+        const observer = new IntersectionObserver(
+          (entries) => {
+            entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+              }
+            });
+          },
+          { threshold: 0.2 }
+        );
+
+        const elements =
+          this.aboutContainer.nativeElement.querySelectorAll('.fade-in');
+        elements.forEach((el: HTMLElement) => observer.observe(el));
+      } else {
+        console.warn('IntersectionObserver non supportato su questo browser.');
+      }
+    }
+  }
+}
